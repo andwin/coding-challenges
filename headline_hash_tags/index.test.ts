78a57567e967eddc59cd5e1f75e8545212bc207f
Anders Windelhed <anders.windelhed@tv4.se>
2025-02-24 20:21:51 +0100

    Lint fixes

--- headline_hash_tags/index.test.ts
+++ headline_hash_tags/index.test.ts
@@ -3,7 +3,7 @@ import getHashTags from './index'
 
 test('headline hash tags - How the Avocado Became the Fruit of the Global Trade', () => {
   const text = 'How the Avocado Became the Fruit of the Global Trade'
-  const expected = ["#avocado", "#became", "#global"]
+  const expected = ['#avocado', '#became', '#global']
 
   const result = getHashTags(text)
 
@@ -12,7 +12,7 @@ test('headline hash tags - How the Avocado Became the Fruit of the Global Trade'
 
 test('headline hash tags - Why You Will Probably Pay More for Your Christmas Tree This Year', () => {
   const text = 'Why You Will Probably Pay More for Your Christmas Tree This Year'
-  const expected = ["#christmas",    "#probably", "#will"]
+  const expected = ['#christmas', '#probably', '#will']
 
   const result = getHashTags(text)
 
@@ -21,7 +21,7 @@ test('headline hash tags - Why You Will Probably Pay More for Your Christmas Tre
 
 test('headline hash tags - Hey Parents, Surprise, Fruit Juice Is Not Fruit', () => {
   const text = 'Hey Parents, Surprise, Fruit Juice Is Not Fruit'
-  const expected = ["#surprise", "#parents", "#fruit"]
+  const expected = ['#surprise', '#parents', '#fruit']
 
   const result = getHashTags(text)
 
@@ -30,7 +30,7 @@ test('headline hash tags - Hey Parents, Surprise, Fruit Juice Is Not Fruit', () 
 
 test('headline hash tags - Visualizing Science', () => {
   const text = 'Visualizing Science'
-  const expected = ["#visualizing", "#science"]
+  const expected = ['#visualizing', '#science']
 
   const result = getHashTags(text)
 
@@ -39,17 +39,16 @@ test('headline hash tags - Visualizing Science', () => {
 
 test('headline hash tags - Minecraft Stars on Youtube Share Secrets to Their Fame', () => {
   const text = 'Minecraft Stars on Youtube Share Secrets to Their Fame'
-  const expected = ["#minecraft", "#youtube", "#secrets"]
+  const expected = ['#minecraft', '#youtube', '#secrets']
 
   const result = getHashTags(text)
 
   assert.deepEqual(result, expected)
 })
 
-
 test('headline hash tags - Are You an Elite Entrepreneur?', () => {
   const text = 'Are You an Elite Entrepreneur?'
-  const expected = ["#entrepreneur", "#elite", "#are"]
+  const expected = ['#entrepreneur', '#elite', '#are']
 
   const result = getHashTags(text)
 
